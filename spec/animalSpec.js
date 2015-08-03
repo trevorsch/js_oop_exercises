5398b17784f44e1925324e1240d6d83782463040
Trevor Scheuermann <trevorsch1@gmail.com>
2015-08-03 14:27:41 -0600

    returns oink if the animal is a pig

--- spec/animalSpec.js
+++ spec/animalSpec.js
@@ -5,50 +5,48 @@ describe('Animal', function(){
 
   beforeEach(function(){
     // create a new animal object each time
-    animal = new Animal("Fido", 27, "Pig");
+    animal = new Animal("Fido", 27, "PiG");
   });
 
   describe('#oink', function(){
     it('should oink if it is a pig', function(){
-
+      expect(animal.oink()).toEqual('Oink');
     });
-    it('should not oink if it is not pig', function(){
+    it('should not oink if xit is not pig', function(){
 
     });
   });
 
   describe('#growUp', function(){
-    it('should increment the age by one', function(){
+    xit('should increment the age by one', function(){
 
     });
   });
 
   describe('#awake', function(){
-    it('should initially be off', function(){
+    xit('should initially be off', function(){
 
     });
   });
 
    describe('#wakeUp', function(){
-    it('should change state to awake', function(){
+    xit('should change state to awake', function(){
 
     });
   });
 
   describe('#sleep', function(){
-    it('should initially be sleeping', function(){
+    xit('should initially be sleeping', function(){
 
     });
   });
 
   describe('#feed', function(){
-    it('should get fed if awake', function(){
+    xit('should get fed if awake', function(){
 
     });
-    it('not should get fed if sleeping', function(){
+    xit('not should get fed if sleeping', function(){
 
     });
   });
 });
-
-
--- src/animal.js
+++ src/animal.js
@@ -1,9 +1,16 @@
-function Animal(){
-
+function Animal(name, age, kind){
+  this.name = name;
+  this.age = age;
+  this.kind = kind;
+  this.awake = false;
 }
 
 Animal.prototype.oink = function(){
-
+  if(this.kind.toUpperCase() === "PIG"){
+    return "Oink";
+  } else {
+    return "Not a pig";
+  }
 };
 
 module.exports=Animal;
